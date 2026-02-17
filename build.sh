bc602c2b85e2d559f084119c429aaad038f95ed8
jacobrluttrull <jacobrluttrull@gmail.com>
2026-02-17 17:47:01 -0500

    fixed site hopefully

--- build.sh
+++ build.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 
-# Generate the static site with default basepath
-python3 src/main.py
-
-# Navigate to docs directory and start server
-cd docs
-python3 -m http.server 8888
+python3 src/main.py "/static_site_generator/"
